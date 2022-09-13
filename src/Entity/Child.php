1d82562bbdff0778eaf5031cc4af4b9f5e151264
Benjamin SUREAU <benjamin.sureau@evaneos.com>
2022-09-13 18:29:25 +0200

    feat: create Child entity

---
+++ src/Entity/Child.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChildRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChildRepository::class)]
+class Child
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $emoji = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $birthDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmoji(): ?int
+    {
+        return $this->emoji;
+    }
+
+    public function setEmoji(int $emoji): self
+    {
+        $this->emoji = $emoji;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+}
