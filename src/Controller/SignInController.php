991b542e564f1a09dde40bcd6db801f3e57f15ae
Benjamin SUREAU <benjamin.sureau@evaneos.com>
2022-09-23 21:26:27 +0200

    feat: add connexion workflow

--- src/Controller/SignInController.php
+++ src/Controller/SignInController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SignInController extends AbstractController
 {
     #[Route('/connexion', name: 'app_sign_in')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        if (true === $this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute("app_dashboard");
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('sign_in/index.html.twig', [
-            'controller_name' => 'SignInController',
+            'last_username'   => $lastUsername,
+            'error'           => $error,
         ]);
     }
 }
--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -3,19 +3,41 @@
 namespace App\Controller;
 
 use App\DTO\Registration;
+use App\Repository\SubscriberRepository;
+use App\Security\EmailVerifier;
 use App\Service\RegistrationService;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SignUpController extends AbstractController
 {
+    public function __construct(
+        private EmailVerifier $emailVerifier,
+        private AuthenticatorInterface $loginAuthenticator
+    ){}
+
     #[Route('/inscription', name: 'app_sign_up')]
-    public function index(Request $request, RegistrationService $registrationService, ValidatorInterface $validator, EntityManagerInterface $entityManager): Response
+    public function index(
+        Request $request,
+        RegistrationService $registrationService,
+        ValidatorInterface $validator,
+        EntityManagerInterface $entityManager,
+        UserAuthenticatorInterface $userAuthenticator
+    ): Response
     {
+        if (true === $this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute("app_dashboard");
+        }
+
         if($request->isMethod('POST')) {
             $registration = new Registration();
             $registration->hydrateFromData($request->request->all());
@@ -38,12 +60,56 @@ class SignUpController extends AbstractController
             $entityManager->persist($subscriber);
             $entityManager->flush();
 
-            //TODO: send welcome email
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $subscriber,
+                (new TemplatedEmail())
+                    ->from(new Address('contact@lerehausseur.fr', 'Le Réhausseur'))
+                    ->to($subscriber->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('sign_up/confirmation_email.html.twig')
+            );
+
+            $userAuthenticator->authenticateUser(
+                $subscriber,
+                $this->loginAuthenticator,
+                $request
+            );
+
             return $this->redirectToRoute('app_dashboard');
         }
 
         return $this->render('sign_up/index.html.twig', [
             'email' => $request->query->get('email')
         ]);
+    }
+
+    #[Route('/verification/email', name: 'app_verify_email')]
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator, SubscriberRepository $subscriberRepository): Response
+    {
+        $id = $request->get('id');
+
+        if (null === $id) {
+            return $this->redirectToRoute('app_sign_up');
+        }
+
+        $user = $subscriberRepository->find($id);
+
+        if (null === $user) {
+            return $this->redirectToRoute('app_sign_up');
+        }
+
+        // validate email confirmation link, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $user);
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+
+            return $this->redirectToRoute('app_sign_up');
+        }
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Votre email a bien été vérifié :)');
+
+        return $this->redirectToRoute('app_sign_up');
     }
 }
