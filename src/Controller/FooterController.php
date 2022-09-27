80a46e3743b924ec74d679cad7b78412ed310f3f
Benjamin SUREAU <benjamin.sureau@evaneos.com>
2022-09-27 12:37:17 +0200

    feat: add footer pages

---
+++ src/Controller/FooterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\Registration;
+use App\Repository\SubscriberRepository;
+use App\Security\EmailVerifier;
+use App\Service\CityService;
+use App\Service\RegistrationService;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class FooterController extends AbstractController
+{
+    #[Route('/mentions-legales', name: 'app_legal_notice')]
+    public function legalNotice(): Response
+    {
+        return $this->render('footer/legal-notice.html.twig');
+    }
+
+    #[Route('/politique-de-confidentialite', name: 'app_privacy_policy')]
+    public function privacyPolicy(): Response
+    {
+        return $this->render('footer/privacy-policy.html.twig');
+    }
+}
